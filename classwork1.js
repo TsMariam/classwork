85499409b8b466c3265e1c2f9ae286aca58e391b
TsMariam <tsaturovamariam@gmail.com>
2019-02-05 21:07:25 +0400

    Add file classwork1. Create first commit

---
+++ classwork1.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+//1.	Given a number. Print “odd” if the number is odd and “even” if it’s even.
+
+let x = 123;
+
+if (x % 2 == 1) {
+    console.log("odd");
+}
+else {
+    console.log("Even");
+}
+
+
+//2.	Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.
+
+let x = 3;
+let y = 14;
+
+if (x === y) {
+    console.log(1);
+} else if (x > y) {
+    if (x % y == 0) {
+        console.log(1);
+    }
+    else {
+        console.log(0);
+    }
+} else {
+    if (y % x == 0) {
+        console.log(1);
+    }
+    else {
+        console.log(0);
+    }
+}
+
